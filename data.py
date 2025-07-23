b7c1093d836a023289acaeafd3e8de480d2a7748
bandama <bandamayan@gmail.com>
2025-07-23 22:46:05 +0200

    Modifications data

--- data.py
+++ data.py
@@ -14,3 +14,23 @@ def get_stock_data(ticker, start_date, end_date):
     stock_data = yf.download(ticker, start=start_date, end=end_date)
     return stock_data
 
+# Fonction pour trier les données
+def sort_stock_data(stock_data, column='Close', ascending=True):
+    """
+    Trie les données boursières par une colonne spécifique.
+    
+    :param stock_data: Un DataFrame contenant les données boursières
+    :param column: La colonne par laquelle trier (par défaut 'Close')
+    :param ascending: Ordre de tri (par défaut True pour croissant)
+    :return: Un DataFrame trié
+    """
+    return stock_data.sort_values(by=column, ascending=ascending)
+    
+# Fonction pour afficher les données
+def display_stock_data(stock_data):
+    """
+    Affiche les données boursières.
+    
+    :param stock_data: Un DataFrame contenant les données boursières
+    """
+    print(stock_data)
