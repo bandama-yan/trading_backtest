17340fb8f717eda9c0a8fafd9d41faa0f9fc0e99
bandama <bandamayan@gmail.com>
2025-09-06 20:11:44 +0200

    Update sma

--- models/sma_strategy.py
+++ models/sma_strategy.py
@@ -3,13 +3,13 @@ def apply_sma_strategy(data, short_window=5, long_window=70):
     df['SMA_short'] = df['Close'].rolling(window=short_window).mean()
     df['SMA_long'] = df['Close'].rolling(window=long_window).mean()
 
-    # On crée un signal : 1 si SMA_short > SMA_long, 0 sinon
+    # Create signal : 1 if SMA_short > SMA_long, 0 else
     df['Signal'] = (df['SMA_short'] > df['SMA_long']).astype(int)
 
-    # La position = Signal du jour précédent pour éviter le look-ahead
+    # position = Signal of the previous day for avoiding look-ahead
     df['Position'] = df['Signal'].shift(1).fillna(0)
 
-    # Retours
+    # Returns
     df['Return'] = df['Close'].pct_change()
     df['Strategy_Return'] = df['Position'] * df['Return']
 
